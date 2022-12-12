03ef2993afa0efcdb6eea438af751dc5cf371149
Sumit Yadav <sumitind108@gmail.com>
2022-12-13 01:38:45 +0530

    added home.jsp in src and created a package controller and class named master controller

--- expensemanager/src/main/java/com/PersonalProject/expensemanager/ExpensemanagerApplication.java
+++ expensemanager/src/main/java/com/PersonalProject/expensemanager/ExpensemanagerApplication.java
@@ -2,8 +2,10 @@ package com.PersonalProject.expensemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages= {"com.PersonalProject.expensemanager.controller"})
 public class ExpensemanagerApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +13,4 @@ public class ExpensemanagerApplication {
 	}
 
 }
+
