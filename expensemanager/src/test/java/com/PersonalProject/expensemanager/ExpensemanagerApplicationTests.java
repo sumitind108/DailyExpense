8b482ff427e0df36aa8636a1b326f1a8efc5ad67
Sumit Yadav <sumitind108@gmail.com>
2022-12-12 17:21:07 +0530

    "First commit for DailyExpense Application"

---
+++ expensemanager/src/test/java/com/PersonalProject/expensemanager/ExpensemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PersonalProject.expensemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
